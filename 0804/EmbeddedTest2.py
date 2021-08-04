13b493f0adab786f7acfc1d6f9b9e1a3d8291459
hans9645 <hans9645@gmail.com>
2021-08-04 20:20:20 +0900

    0804 learning

--- 0804/EmbeddedTest2.py
+++ 0804/EmbeddedTest2.py
@@ -3,9 +3,8 @@ fname='/home/yuto/Documents/AIhub/download.jpeg'
 original=cv2.imread(fname,cv2.IMREAD_COLOR)
 
 b,g,r=cv2.split(original)
-#b=0
-original=cv2.merge((b,g,r))
+original[:,:,0]=0 # there are three color channel so, it allows only two channel to make yellow picture
 cv2.imshow('Image',original)
-#cv2.imwrite('file3.png',original)
+cv2.imwrite('file3.png',original)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
